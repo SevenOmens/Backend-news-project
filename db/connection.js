d3766f5e1affe48a18e6e031bb449931751cf20f
SevenOmens <ajpbmusic@gmail.com>
2022-08-17 14:30:00 +0100

    help

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
 const config =
-  ENV === 'production'
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -10,13 +13,8 @@ const config =
       }
     : {};
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
 
 module.exports = new Pool(config);
